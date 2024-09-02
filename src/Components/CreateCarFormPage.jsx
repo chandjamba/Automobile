86e34ae0e7a5a5284eb94f771542a1e62309244c
chandjamba <chandjamba2001@gmail.com>
2024-09-02 03:58:54 -0400

    working on radio buttons of form

--- src/Components/CreateCarFormPage.jsx
+++ src/Components/CreateCarFormPage.jsx
@@ -4,9 +4,13 @@ import { databases } from "../lib/appwrite";
 
 export default function CreateCarFormPage() {
   const [carModels, setCarModels] = useState();
-  const [carTypes, setCarTypes] = useState();
+  const [selectedCarModel, setSelectedCarModel] = useState();
   const [carMakes, setCarMakes] = useState();
-  const [carColor, setCarColor] = useState();
+  const [selectedCarMake, setSelectedCarMake] = useState();
+  const [selectedCarColor, setSelectedCarColor] = useState();
+  const [carTypes, setCarTypes] = useState();
+  const [selectedCarType, setSelectedCarType] = useState();
+  const [selectedCarEngineType, setSelectedCarEngineType] = useState();
 
   useEffect(() => {
     function getModels() {
@@ -68,26 +72,6 @@ export default function CreateCarFormPage() {
     getMakes();
   }, []);
 
-  useEffect(() => {
-    function getColor() {
-      try {
-        databases
-          .listDocuments(
-            import.meta.env.VITE_APPWRITE_DATABASE_ID,
-            import.meta.env.VITE_APPWRITE_CAR_COLOR_COLLECTION_ID,
-            []
-          )
-          .then((resp) => {
-            setCarColor(resp?.documents);
-            console.log(resp?.documents);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getColor();
-  }, []);
-
   return (
     <div className="create__car">
       <form className="create__car-form">
@@ -96,14 +80,30 @@ export default function CreateCarFormPage() {
         </div>
         <div className="create__car-appearance-box">
           <p className="create__car-model-input-heading">Model</p>
-          <select className="create__car-model-input">
+          <select
+            className="create__car-model-input"
+            name="carModel"
+            value={selectedCarModel?.carModel}
+            onChange={(event) => {
+              console.log(event.target.value);
+              setSelectedCarModel(event.target.value);
+            }}
+          >
             {carModels?.map((carModel) => {
               return <option key={carModel?.$id}>{carModel?.modelName}</option>;
             })}
           </select>
           <p className="create__car-make-input-heading">Make</p>
 
-          <select className="create__car-make-input">
+          <select
+            className="create__car-make-input"
+            name="carMake"
+            value={selectedCarMake?.carMake}
+            onChange={(event) => {
+              console.log(event.target.value);
+              setSelectedCarMake(event.target.value);
+            }}
+          >
             {carMakes?.map((carMake) => {
               return (
                 <option key={carMake.$id}>{carMake?.makeCompanyName}</option>
@@ -116,16 +116,11 @@ export default function CreateCarFormPage() {
             className="create__car-color-input"
             type="text"
             placeholder="Enter here..."
-            value={carColor?.colorName}
+            name="carColor"
+            value={selectedCarColor?.colorName}
             onChange={(event) => {
               console.log(event.target.value);
-
-              setCarColor((pureState) => {
-                return {
-                  ...pureState,
-                  color: event.target.value,
-                };
-              });
+              setSelectedCarColor(event.target.value);
             }}
           />
           <p className="create__car-seats-input-heading">Seats</p>
@@ -133,7 +128,15 @@ export default function CreateCarFormPage() {
           <input className="create__car-seats-input" type="number" />
         </div>
         <p className="create__car-type-input-headings">Select Type</p>
-        <select className="create__car-type-input">
+        <select
+          className="create__car-type-input"
+          name="carType"
+          value={selectedCarType?.carType}
+          onChange={(event) => {
+            console.log(event.target.value);
+            setSelectedCarType(event.target.value);
+          }}
+        >
           {carTypes?.map((carType) => {
             return <option key={carType.$id}>{carType?.typeName}</option>;
           })}
@@ -143,11 +146,31 @@ export default function CreateCarFormPage() {
           Gear Transmission
         </p>
         <div className="create__car-engine-type-input-container">
-          <input className="create__car-engine-type-input-1" type="radio" />
+          <input
+            className="create__car-engine-type-input-1"
+            type="radio"
+            name="Automatic"
+            value={"automatic"}
+            checked={event.target.value === "automatic"}
+            onChange={(event) => {
+              console.log(event.target.value);
+              setSelectedCarEngineType(event.target.value);
+            }}
+          />
           <label className="create__car-engine-type-input-1-heading">
             Automatic
           </label>
-          <input className="create__car-engine-type-input-2" type="radio" />
+          <input
+            className="create__car-engine-type-input-2"
+            type="radio"
+            name="Manual"
+            value={"manual"}
+            checked={event.target.value === "manual"}
+            onChange={(event) => {
+              console.log(event.target.value);
+              setSelectedCarEngineType(event.target.value);
+            }}
+          />
           <label className="create__car-engine-type-input-2-heading">
             Manual
           </label>
