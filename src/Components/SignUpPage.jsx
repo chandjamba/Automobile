217da406bdc743138c9a2bc7ee52ab1e96eb8b34
chandjamba <chandjamba2001@gmail.com>
2024-10-22 01:01:19 -0400

    Start work on auth at signup page

--- src/Components/SignUpPage.jsx
+++ src/Components/SignUpPage.jsx
@@ -20,9 +20,9 @@ export default function SignUpPage() {
       formDataObject.enterEmail,
       formDataObject.enterPassword
     );
-    // if (createdAccount.$id) {
-    //   navigate("/verify-email");
-    // }
+    if (createdAccount.$id) {
+      navigate("/verify-email");
+    }
 
     console.log(createdAccount);
   };
