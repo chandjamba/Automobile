cd78e62c930e388bf00d1eb19cb86da9985d2479
chandjamba <chandjamba2001@gmail.com>
2025-02-03 22:08:54 -0500

    working on authcontext

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,16 +1,11 @@
-import {
-  createContext,
-  useContext,
-  useLayoutEffect,
-  useState,
-} from "react";
+import { createContext, useContext, useLayoutEffect, useState } from "react";
 import { account } from "../lib/appwrite";
 import { useNavigate } from "react-router-dom";
 //Step1 Create context. //
 export const AuthContext = createContext();
 
 // third step is to export a components that accepts children and pass them to
-<Context.Provider value={{}}> {children}</Context.Provider>;
+
 export function AuthContextProvider({ children }) {
   const [logedInUser, setLogedInUser] = useState();
   const navigate = useNavigate();
